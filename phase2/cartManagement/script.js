00929f3cee43472ad06ce4db8bcf0967b6a78ee3
eliojr2000 <eliojr20001@gmail.com>
2021-08-11 17:21:48 -0500

    Finished Cart Management

--- phase2/cartManagement/script.js
+++ phase2/cartManagement/script.js
@@ -1,44 +1,90 @@
-var Item = /** @class */ (function () {
-    function Item(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-    return Item;
-}());
-var Cart = /** @class */ (function () {
-    function Cart() {
-        this.items = [];
-        this.count = 0;
-    }
-    Cart.prototype.add = function () {
-        var added = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            added[_i] = arguments[_i];
-        }
-        for (var i = 0; i < added.length; i++) {
-            this.items.push(added[i]);
-            this.count++;
-        }
-    };
-    Cart.prototype.size = function () {
-        return this.count;
-    };
-    Cart.prototype.getItems = function () {
-        return this.items;
-    };
-    Cart.prototype.printItems = function () {
-        for (var i = 0; i < this.items.length; i++) {
-            console.log("Item: " + this.items[i].name + " | Price: $" + this.items[i].price);
-        }
-    };
-    return Cart;
-}());
 function test() {
-    var x = new Cart();
-    var iphone = new Item("iphone", 50);
-    var banana = new Item("banana", 1);
-    x.add(iphone, banana);
-    x.printItems();
-    console.log();
 }
-test();
+function addItem(item, price) {
+    var text = "";
+    var index = -1;
+    if (sessionStorage.getItem("cart") == null) {
+        text = '{}';
+        index = 0;
+    }
+    else {
+        text = sessionStorage.getItem("cart");
+        index = parseInt(sessionStorage.getItem("index"));
+    }
+    var cart = JSON.parse(text);
+    var newEntry = {
+        "name": item,
+        "price": price
+    };
+    cart[index] = newEntry;
+    console.log(cart);
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    sessionStorage.setItem("index", (index + 1).toString());
+    //Update the counter in the page
+    document.getElementById("cartSize").innerHTML = "Cart Size: " + (index + 1);
+}
+function checkoutItems() {
+    //Get info from our localStorage
+    var cart = JSON.parse(sessionStorage.getItem("cart"));
+    if (cart == null) {
+        var x = document.createElement('h3');
+        x.innerHTML = "There are no items in the cart";
+        document.getElementById('emptyCartMessage').appendChild(x);
+        return;
+    }
+    var index = parseInt(sessionStorage.getItem("index"));
+    //Here we will add out the total price
+    var total = 0;
+    //Create the table, its head (container) and body (container)
+    var table = document.createElement('table');
+    var thead = document.createElement('thead');
+    var tbody = document.createElement('tbody');
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    //Create the heading and add to the head container
+    var heading = document.createElement("tr");
+    var a = document.createElement("th");
+    var b = document.createElement("th");
+    a.innerHTML = "Item";
+    b.innerHTML = "Price";
+    heading.appendChild(a);
+    heading.appendChild(b);
+    thead.appendChild(heading);
+    //Now that we done the header, we
+    //do the table's body now
+    for (var i = 0; i < index; i++) {
+        //Retrieves the data from the JSON Object
+        var itemName = cart[i].name;
+        var price = parseInt(cart[i].price);
+        //Add to the total
+        total = total + price;
+        //Creates a row and the row elements
+        var row = document.createElement("tr");
+        var x = document.createElement("td");
+        var y = document.createElement("td");
+        //Add text to row elements
+        x.innerHTML = itemName;
+        y.innerHTML = "$" + price.toLocaleString("en-US");
+        //Add row elements to the whole row element
+        row.appendChild(x);
+        row.appendChild(y);
+        //Finally, add the newly created row to the table's body
+        tbody.appendChild(row);
+    }
+    document.getElementById('total').innerHTML = "Total: $" + total.toLocaleString("en-US");
+    //Add the table to the DOM
+    document.getElementById('checkoutTable').appendChild(table);
+}
+function clearCart() {
+    sessionStorage.removeItem("cart");
+    sessionStorage.removeItem("index");
+    window.location.reload();
+}
+window.onload = function () {
+    var t = document.getElementsByTagName("title")[0];
+    var x = parseInt(sessionStorage.getItem("index"));
+    if (t.innerHTML == "Checkout")
+        checkoutItems();
+    else if (t.innerHTML == "Store" && !isNaN(x))
+        document.getElementById("cartSize").innerHTML = "Cart Size: " + x;
+};
--- phase2/cartManagement/script.ts
+++ phase2/cartManagement/script.ts
@@ -1,53 +1,127 @@
-class Item
-{
-    constructor
-    (
-        public name: string,
-        public price: number,
-    ){}
-}
-
-class Cart
-{
-    private items: Item[] = [];
-    private count = 0;
-
-    public add(...added: Item[])
-    {
-        for(var i = 0; i < added.length; i++)
-        {
-            this.items.push(added[i]);
-            this.count++;
-        }
-    }
-
-    public size()
-    {
-        return this.count;
-    }
-
-    public getItems()
-    {
-        return this.items;
-    }
-
-    public printItems()
-    {
-        for(var i = 0; i < this.items.length; i++)
-        {
-            console.log("Item: " + this.items[i].name + " | Price: $" + this.items[i].price);
-        }
-    }
-}
-
 function test()
 {
-    var x = new Cart();
-    var iphone = new Item("iphone", 50);
-    var banana = new Item("banana", 1);
-
-    x.add(iphone, banana);
-    x.printItems();
+    
 }
 
-test();
+function addItem(item: string, price: number)
+{   
+    let text: string = "";
+    let index: number = -1;
+
+    if(sessionStorage.getItem("cart") == null)
+    {
+        text = '{}'
+        index = 0;
+    }
+    else
+    {
+        text = sessionStorage.getItem("cart");
+        index = parseInt(sessionStorage.getItem("index"));
+    }
+
+    let cart: JSON = JSON.parse(text);
+
+    let newEntry = 
+    {
+        "name": item,
+        "price": price
+    };
+
+    cart[index] = newEntry;
+
+    console.log(cart);
+
+    sessionStorage.setItem('cart', JSON.stringify(cart))
+    sessionStorage.setItem("index", (index+1).toString());
+
+    //Update the counter in the page
+    document.getElementById("cartSize").innerHTML = "Cart Size: " + (index+1);
+}
+
+function checkoutItems()
+{
+    //Get info from our localStorage
+    let cart = JSON.parse(sessionStorage.getItem("cart"));
+    if(cart == null)
+    {   
+        let x = document.createElement('h3');
+        x.innerHTML = "There are no items in the cart";
+        document.getElementById('emptyCartMessage').appendChild(x);
+        return;
+    }
+    let index = parseInt(sessionStorage.getItem("index"));
+
+    //Here we will add out the total price
+    let total: number = 0;
+
+    //Create the table, its head (container) and body (container)
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    //Create the heading and add to the head container
+    let heading = document.createElement("tr");
+    let a = document.createElement("th");
+    let b = document.createElement("th");
+
+    a.innerHTML = "Item";
+    b.innerHTML = "Price";
+
+    heading.appendChild(a);
+    heading.appendChild(b);
+
+    thead.appendChild(heading);
+
+    //Now that we done the header, we
+    //do the table's body now
+    for(let i = 0; i < index; i++)
+    {
+        //Retrieves the data from the JSON Object
+        let itemName: string = cart[i].name;
+        let price: number = parseInt(cart[i].price);
+
+        //Add to the total
+        total = total + price;
+
+        //Creates a row and the row elements
+        let row = document.createElement("tr");
+        let x = document.createElement("td");
+        let y = document.createElement("td");
+
+        //Add text to row elements
+        x.innerHTML = itemName;
+        y.innerHTML = "$" + price.toLocaleString("en-US");
+
+        //Add row elements to the whole row element
+        row.appendChild(x);
+        row.appendChild(y);
+
+        //Finally, add the newly created row to the table's body
+        tbody.appendChild(row);
+    }
+
+    document.getElementById('total').innerHTML = "Total: $" + total.toLocaleString("en-US");
+
+    //Add the table to the DOM
+    document.getElementById('checkoutTable').appendChild(table);
+}
+
+function clearCart()
+{
+    sessionStorage.removeItem("cart");
+    sessionStorage.removeItem("index");
+    window.location.reload();
+}
+
+window.onload = function()
+{
+    let t = document.getElementsByTagName("title")[0];
+    let x = parseInt(sessionStorage.getItem("index"));
+
+    if(t.innerHTML == "Checkout")
+        checkoutItems();
+    else if(t.innerHTML == "Store" && !isNaN(x))
+        document.getElementById("cartSize").innerHTML = "Cart Size: " + x;
+}
